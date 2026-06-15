ac1fea0594d6446d5d2eb520c8fa88453e9e70ae
Deep Shah <deepshah@mac.lan>
2026-06-15 14:47:09 -0400

    Restructure into fitness-microservice, add userservice

---
+++ fitness-microservice/userservice/src/main/java/com/fitness/userservice/User.java
@@ -0,0 +1,35 @@
+package com.fitness.userservice.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Entity 
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
